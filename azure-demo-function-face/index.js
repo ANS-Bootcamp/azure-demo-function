d38048851ef345bcdce36a2084018a736a541e98
nathanguk <nathan.gaskill@ansgroup.co.uk>
2018-03-05 14:21:03 +0000

    updates

--- azure-demo-function-face/index.js
+++ azure-demo-function-face/index.js
@@ -67,9 +67,6 @@ module.exports = function (context, myBlob) {
                         "colours": {
                             "value": data.color.dominantColors.join(', ')
                         },
-                        "celebrities": {
-                            "value": data.celebrities.join(', ')
-                        },
                         "landmarks": {
                             "value": data.landmarks.join(', ')
                         },

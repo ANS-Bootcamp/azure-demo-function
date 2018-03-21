cfd524a1596818e3f6441204911946b81941ca4f
nathanguk <nathan.gaskill@ansgroup.co.uk>
2018-03-21 22:21:54 +0000

    updated face function

--- azure-demo-function-face/index.js
+++ azure-demo-function-face/index.js
@@ -13,8 +13,18 @@ module.exports = function (context, myBlob) {
     //Split url path
     imageUriArray = imageUriArray[1].split("/")
 
+    //Create Blob Service
+    var blobService = azure.createBlobService();
+
+    //Creates container if not exists
+    blobService.createContainerIfNotExists(api, {publicAccessLevel : 'blob'}, function(error) {
+    if(error) {
+        context.log(error);
+    }
+
     //Replace "images" container to "thumbs"
     imageUriArray[1] = "thumbs"
+
     //Build url path
     var thumbsPath = imageUriArray.join("/");
     var thumbUri = "https://" + thumbsPath;

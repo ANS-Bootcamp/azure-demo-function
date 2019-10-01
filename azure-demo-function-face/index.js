7e133bfa2f07ecab5f5d75b1fccb929ec1f88dfa
Nathan Gaskill <nathang@ANSGRP-002830.local>
2019-10-01 17:14:30 +0100

    Azure Functions v2

--- azure-demo-function-face/index.js
+++ azure-demo-function-face/index.js
@@ -39,8 +39,14 @@ module.exports = async function (context, myBlob) {
     throw new Error('please set/export the following environment variables: ' + keyCognitive + ' ' + keyRegion);
     }
 
-    let serviceKey = process.env[keyCognitive];
-    let region =process.env[keyRegion];
+    //let serviceKey = process.env[keyCognitive];
+    //let region =process.env[keyRegion];
+    context.log(process.env[keyCognitive]);
+    context.log(process.env[keyRegion]);
+
+    let serviceKey = "37326ff1ada949d4b3d7eb3a8154cdf4";
+    let region = "westeurope";
+
     let endpoint =  'https://'+region +'.api.cognitive.microsoft.com';
     
     context.log(endpoint);
@@ -53,6 +59,7 @@ module.exports = async function (context, myBlob) {
     //image query
     function imageQuery(){
         context.log("Calling Face API")
+
         faceApiClient.face.detectWithStream(myBlob, {returnFaceAttributes: ['age','gender','smile','facialHair','glasses','emotion','hair','makeup']})
         
             .then(function(data){    
